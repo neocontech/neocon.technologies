2efabce5878d2f4d843caadbbca8cd90623282fe
Neocon Technologies LTD <neocontechltd@gmail.com>
2023-07-10 12:00:19 +0600

    CU-85ztfvkpw - Frontend : Landing page Design

---
+++ components/Homepage/Hero.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Image from "next/image";
+// import smallhero from "../../public/assets/homepage/smallhoemheroasset.png";
+import largehero2 from "../../public/assets/homepage/largehomeheroasset2.png";
+import largeHomehero from "../../public/assets/homepage/largehomeheroasset.png";
+// import smallhero2 from "../../public/assets/homepage/smallhomeheroasset2.png";
+// import Partner1 from "../../public/assets/homepage/kabirlogo.png";
+// import Partner1 from "../../public/assets/homepage/ucblogo.png";
+
+function Hero() {
+  return (
+    <>
+      <div className="px-10 2xl:px-80 xsm:px-5 sm:px-5">
+        <div className="">
+          <div className="landing_bg">
+            <div className="flex justify-between xsm:flex-col-reverse sm:flex-col-reverse pt-40 pb-16 px-5">
+              <div className="flex flex-row relative my-auto w-full">
+                <div className="absolute z-0  left-80 xsm:left-0 sm:left">
+                  <Image
+                    src={largehero2}
+                    alt="large hero .png"
+                    className="w-auto h-auto"
+                  />
+                </div>
+                <div className="text-text_84 xsm:text-text_41 sm:text-text_41 text-ntl_black font-semibold leading-tight z-10 ">
+                  <p className="text-ntl_orange">Fintech</p>
+                  <p>Development</p>
+                  <p>Platform</p>
+                  <button className="bg-ntl_black rounded-3xl text-ntl_white ">
+                    <p className="text-base px-10 py-5">Get in Touch</p>
+                  </button>
+                </div>
+              </div>
+              <div className="w-3/5 xsm:w-full sm:w-full">
+                <Image
+                  src={largeHomehero}
+                  alt="large home hero .png"
+                  className="w-auto h-auto"
+                />
+              </div>
+            </div>
+            <p className="text-sm text-ntl_black font-medium px-5 pb-5">
+              Trusted by over <span className="text-ntl_orange"> 100+</span>
+              Stock Brokerage House <br />
+              and Enterprise Companies:
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { FiCircle, FiX } from "react-icons/fi";
+import { HiMenuAlt3 } from "react-icons/hi";
+import NeoconTechnologies from "../../public/assets/navbar/navbarlogo.png";
+
+function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <>
+      <div className="sticky z-50 top-0 left-0 right-0 w-full">
+        <div className="px-10 2xl:px-80 xsm:px-5 sm:px-5 bg-ngl_white/60 backdrop-blur-sm backdrop-filter">
+          <div className="py-5">
+            <div className="flex justify-between">
+              <Link href="/" className="cursor-pointer">
+                <Image
+                  src={NeoconTechnologies}
+                  alt="neocon group.png"
+                  className="w-44  h-auto"
+                />
+              </Link>
+              <div className="flex flex-row my-auto">
+                <div className="hidden xsm:block sm:block">
+                  {isMobileMenuOpen ? (
+                    <FiX
+                      className="my-auto text-2xl text-ngl_black"
+                      onClick={toggleMobileMenu}
+                    />
+                  ) : (
+                    <HiMenuAlt3
+                      className="my-auto text-2xl text-black"
+                      onClick={toggleMobileMenu}
+                    />
+                  )}
+                </div>
+                <div className="xsm:hidden sm:hidden flex flex-row xsm:flex-col sm:flex-col my-auto">
+                  <Link href="/company" className="flex flex-row mx-5">
+                    <FiCircle
+                      size={10}
+                      className="my-auto text-xs text-ngl_orange"
+                    />
+                    <p className="pl-2 font-medium">Company</p>
+                  </Link>
+                  <Link href="/business" className="flex flex-row mx-5">
+                    <FiCircle
+                      size={10}
+                      className="my-auto text-xs text-ngl_orange"
+                    />
+                    <p className="pl-2 font-medium">Our Business</p>
+                  </Link>
+                  <Link href="/news" className="flex flex-row mx-5">
+                    <FiCircle
+                      size={10}
+                      className="my-auto text-xs text-ngl_orange"
+                    />
+                    <p className="pl-2 font-medium">News Room</p>
+                  </Link>
+                  <Link href="/contact-us" className="flex flex-row mx-5">
+                    <FiCircle
+                      size={10}
+                      className="my-auto text-xs text-ngl_orange"
+                    />
+                    <p className="pl-2 font-medium">Contact Us</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {isMobileMenuOpen && (
+              <div className="md:hidden">
+                <Link href="/company" className="block my-3 mx-5">
+                  <p className="font-medium">Company</p>
+                </Link>
+                <Link href="/business" className="block my-3 mx-5">
+                  <p className="font-medium">Our Business</p>
+                </Link>
+                <Link href="/news" className="block my-3 mx-5">
+                  <p className="font-medium">News Room</p>
+                </Link>
+                <Link href="/contact-us" className="block my-3 mx-5">
+                  <p className="font-medium">Contact Us</p>
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
