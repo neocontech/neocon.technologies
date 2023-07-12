a20bc58c5cf6e8a0030188b09f770e94716dda6b
Neocon Technologies LTD <neocontechltd@gmail.com>
2023-07-12 11:15:38 +0600

    CU-85ztfvkpw - Frontend : Our Products page added

---
+++ components/Homepage/OurProducts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function OurProducts() {
+  return (
+    <>
+    <div className='px-10 2xl:px-80 xsm:px-5 sm:px-5'>
+        <div className='pb-10'>
+            
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default OurProducts
