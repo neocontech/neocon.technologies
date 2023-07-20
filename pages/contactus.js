5413e05cea9dea3dcc071908231b996501424b5c
Neocon Technologies LTD <neocontechltd@gmail.com>
2023-07-20 12:29:23 +0600

    CU-85ztjndeb - Frontend: Contact us page

---
+++ pages/contactus.js
@@ -0,0 +1,18 @@
+import Applayout from "@/components/Layout/Applayout";
+
+import React from "react";
+import PageHead from "./PageHead";
+
+function services() {
+    const pageTitle = "Neocon Technologies Limited / Contact Us";
+  return (
+    <>
+    <PageHead title={pageTitle} />
+      <Applayout>
+        
+      </Applayout>
+    </>
+  );
+}
+
+export default services;
