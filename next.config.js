ea5adfcc5b72438918781bb87495c4549600a857
Neocon Technologies LTD <neocontechltd@gmail.com>
2023-07-23 12:11:05 +0600

    director and team backend integration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**.neocongroup.com",
+//       },
+//     ],
+//   },
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["localhost", "127.0.0.1"],
+  },
+};
+
+module.exports = nextConfig;
